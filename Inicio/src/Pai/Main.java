ec1dc809e8d3a902a78d99c778eeefc631d5be76
YuriKauan10 <yurrikauan@gmail.com>
2025-03-22 16:51:19 -0300

    Segundo commit teste

--- Inicio/src/Pai/Main.java
+++ Inicio/src/Pai/Main.java
@@ -1,16 +1,16 @@
 package Pai;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		ArrayList<Integer> numeros = new ArrayList<>();
 		
-		
+		int n = sc.nextInt();
+		int mat[][] = new int[n][n];
+		mat[0][1] = sc.nextInt();
+		System.out.println(mat[0][1]);
 		sc.close();
 	}
 
